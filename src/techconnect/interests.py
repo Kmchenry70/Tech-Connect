b0b098e505dd17ccbc6322de7a0572937cbc60f0
Sam Cook <ob1yoshi2@gmail.com>
2024-05-06 11:56:38 -0500

    Made it where the user gets a message to let him/her know that the form needs at least one selection

--- src/techconnect/interests.py
+++ src/techconnect/interests.py
@@ -11,18 +11,24 @@ bp = Blueprint('interests', __name__) # url_prefix='/interests'
 @bp.route('/interests', methods=('GET', 'POST'))
 def interests(): 
     template_name = 'app/interests.html'
-    interests = []
     if request.method == 'POST':
-        interests.append(request.form('interest'))
+        sports = request.form.getlist('sports')
+        ministries = request.form.getlist('ministries')
+        STEM = request.form.getlist('STEM')
+        clubs = request.form.getlist('clubs')
+
         db = get_db()
         error = None
-
-        if len(interests) == 0: 
+        # This code will run though and check to see if there has been anything filled for these interest. 
+        # If none of the interests have at least one selection, then it will store a message that reminds the user to select at least one.
+        if (sports is None) and (ministries is None) and (STEM is None) and (clubs is None): 
             error = 'Need to select at least one of the options.'
 
         if error is None: 
-            return redirect(url_for("home.home"))
+            # Need to store data from sports, ministries, STEM, and clubs into the user's database
 
+            return redirect(url_for("home.home"))
+            
         flash(error)
 
     return render_template(template_name, template_name=template_name)
