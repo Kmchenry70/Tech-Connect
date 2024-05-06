7f0af93b60595a31bc4b380d2eba2272cb3f6006
Sam Cook <ob1yoshi2@gmail.com>
2024-05-06 11:36:28 -0500

    trying to flash a message to select an interest

--- src/techconnect/interests.py
+++ src/techconnect/interests.py
@@ -11,9 +11,19 @@ bp = Blueprint('interests', __name__) # url_prefix='/interests'
 @bp.route('/interests', methods=('GET', 'POST'))
 def interests(): 
     template_name = 'app/interests.html'
+    interests = []
     if request.method == 'POST':
-        # submit = 
-        return redirect(url_for("home.home"))
+        interests.append(request.form('interest'))
+        db = get_db()
+        error = None
+
+        if len(interests) == 0: 
+            error = 'Need to select at least one of the options.'
+
+        if error is None: 
+            return redirect(url_for("home.home"))
+
+        flash(error)
 
     return render_template(template_name, template_name=template_name)
         
