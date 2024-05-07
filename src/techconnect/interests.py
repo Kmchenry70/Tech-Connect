05c4c11cce276c4c2da3dd3f345db75359526f0b
Sam Cook <ob1yoshi2@gmail.com>
2024-05-07 09:13:48 -0500

    trying a way to store interests

--- src/techconnect/interests.py
+++ src/techconnect/interests.py
@@ -21,16 +21,43 @@ def interests():
         error = None
         # This code will run though and check to see if there has been anything filled for these interest. 
         # If none of the interests have at least one selection, then it will store a message that reminds the user to select at least one.
-        if (sports is None) and (ministries is None) and (STEM is None) and (clubs is None): 
+        if not (sports or ministries or STEM or clubs): 
             error = 'Need to select at least one of the options.'
 
         if error is None: 
             # Need to store data from sports, ministries, STEM, and clubs into the user's database
+            user_id = g.user['id']
+
+            try:
+                # Start transaction
+                db.execute('BEGIN')
+
+                # Clear existing interests
+                db.execute(
+                    'DELETE FROM user_interests WHERE user_id = ?',
+                    (user_id,)
+                )
+                
+                # Compile all interests into one list
+                interests = sports + ministries + STEM + clubs
+                
+                # Insert new interests
+                for interest in interests:
+                    db.execute(
+                        'INSERT INTO user_interests (user_id, interest) VALUES (?, ?)',
+                        (user_id, interest)
+                    )
+                
+                # Commit transaction
+                db.commit()
+            except Exception as e:
+                # Roll back in case of error
+                db.rollback()
+                raise e
 
             return redirect(url_for("home.home"))
-            
+
         flash(error)
 
     return render_template(template_name, template_name=template_name)
         
-
