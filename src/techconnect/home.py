4a06972a77637b8ef06fb74b68f45bc3863bbb55
Kmchenry70 <kmchenrysh70@gmail.com>
2024-05-07 18:05:23 -0500

    started home backend

--- src/techconnect/home.py
+++ src/techconnect/home.py
@@ -6,16 +6,16 @@ from werkzeug.exceptions import abort
 from techconnect.auth import login_required
 
 from techconnect.db import get_db
-# from techconnect.db import Clubs, Events
+from techconnect.db import Clubs, Events
 
 bp = Blueprint('home', __name__)
 
 @bp.route('/')
 def home():
-    # clubs = Clubs.query.all()
+    template_name = 'app/home.html'
 
     clubs = ['ACE', 'SWE', "IEEE", 'ACM']
 
     # events = Events.query.all()
 
-    return render_template("app/home.html")
+    return render_template(template_name, template_name=template_name)
